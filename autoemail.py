2ed5b892f85205d564985caecd51623c3f24a135
Jaggar <89122368+XuejiFang@users.noreply.github.com>
2022-03-20 22:23:27 +0800

    Update autoemail.py

--- autoemail.py
+++ autoemail.py
@@ -1,50 +1,35 @@
+# smtplib 用于邮件的发信动作
 import smtplib
-import email
-# 负责构造文本
 from email.mime.text import MIMEText
-# 负责构造图片
-from email.mime.image import MIMEImage
-# 负责将多个对象集合起来
-from email.mime.multipart import MIMEMultipart
+# email 用于构建邮件内容
 from email.header import Header
 
-# SMTP服务器,这里使用163邮箱
-mail_host = "smtp.qq.com"
-# 发件人邮箱
-mail_sender = "1213719606@qq.com"
-# 邮箱授权码,注意这里不是邮箱密码,如何获取邮箱授权码,请看本文最后教程
-mail_license = "gxipqdrqcaptbach"
-# 收件人邮箱，可以为多个收件人
-mail_receivers = ["Jaggar.Fang@hotmail.com","XuejiFang_SHU@outlook.com"]
+# 用于构建邮件头
 
-mm = MIMEMultipart('related')
+# 发信方的信息：发信邮箱，QQ 邮箱授权码
+from_addr = '1213719606@qq.com'
+password = 'gxipqdrqcaptbach'
 
-# 邮件主题
-subject_content = """Python邮件测试"""
-# 设置发送者,注意严格遵守格式,里面邮箱为发件人邮箱
-mm["From"] = "sender_name<1213719606@.com>"
-# 设置接受者,注意严格遵守格式,里面邮箱为接受者邮箱
-mm["To"] = "receiver_1_name<Jaggar.Fang@hotmail.com>,receiver_2_name<XuejiFang_SHU@outlook.com>"
-# 设置邮件主题
-mm["Subject"] = Header(subject_content,'utf-8')
+# 收信方邮箱
+to_addr = 'Jaggar.Fang@hotmail.com'
 
-# 邮件正文内容
-body_content = """你好，这是一个测试邮件！"""
-# 构造文本,参数1：正文内容，参数2：文本格式，参数3：编码方式
-message_text = MIMEText(body_content,"plain","utf-8")
-# 向MIMEMultipart对象中添加文本对象
-mm.attach(message_text)
+# 发信服务器
+smtp_server = 'smtp.qq.com'
 
-# 创建SMTP对象
-stp = smtplib.SMTP()
-# 设置发件人邮箱的域名和端口，端口地址为25
-stp.connect(mail_host, 25)
-# set_debuglevel(1)可以打印出和SMTP服务器交互的所有信息
-stp.set_debuglevel(1)
-# 登录邮箱，传递参数1：邮箱地址，参数2：邮箱授权码
-stp.login(mail_sender,mail_license)
-# 发送邮件，传递参数1：发件人邮箱地址，参数2：收件人邮箱地址，参数3：把邮件内容格式改为str
-stp.sendmail(mail_sender, mail_receivers, mm.as_string())
-print("邮件发送成功")
-# 关闭SMTP对象
-stp.quit()
+# 邮箱正文内容，第一个参数为内容，第二个参数为格式(plain 为纯文本)，第三个参数为编码
+msg = MIMEText('send by python', 'plain', 'utf-8')
+
+# 邮件头信息
+msg['From'] = Header(from_addr)
+msg['To'] = Header(to_addr)
+msg['Subject'] = Header('python test')
+
+# 开启发信服务，这里使用的是加密传输
+server=smtplib.SMTP_SSL(smtp_server)
+server.connect(smtp_server, 465)
+# 登录发信邮箱
+server.login(from_addr, password)
+# 发送邮件
+server.sendmail(from_addr, to_addr, msg.as_string())
+# 关闭服务器
+server.quit()
